ecfb7c87defc77323bcd7147a0fe86d71a3d5195
Pavel Kastsiuk <pavel_kastsiuk1@epam.com>
2024-04-09 11:16:53 +0200

    azure js task 3

---
+++ data.ts
@@ -0,0 +1,22 @@
+import { Product } from "./types";
+
+export const list: Product[] = [
+  {
+    id: "1",
+    title: "Tesla",
+    description: "Electro",
+    price: 70000,
+  },
+  {
+    id: "2",
+    title: "Kia",
+    description: "Mechanic",
+    price: 20000,
+  },
+  {
+    id: "3",
+    title: "BMW",
+    description: "Hybrid",
+    price: 30000,
+  },
+];
